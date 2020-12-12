f233fce35830f8fe3be0a713d2ad2b22e72013d7
ajaidanial <ajai@geopits.com>
2020-12-12 11:45:59 +0530

    added the ping endpoint

--- server.go
+++ server.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// server instance
+	server := echo.New()
+
+	// url mapping
+	server.GET("/ping", pingHandler)
+
+	// running the server
+	server.Logger.Fatal(server.Start(":8080"))
+}
+
+/**
+Handler for the `/ping` endpoint. This is used as a status check for the application.
+Like, if in case this is needed for anything.
+*/
+func pingHandler(context echo.Context) error {
+	return context.String(http.StatusOK, "pong")
 }
