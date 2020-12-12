7fb1bc03216391f508cc9aec505f2004b2b9721d
ajaidanial <ajai@geopits.com>
2020-12-12 12:48:42 +0530

    added the read config function

--- server.go
+++ server.go
@@ -1,10 +1,42 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
+
+// Configuration defines the single config dict from the config.json file.
+type Configuration struct {
+	Host    string   `json:"host"`
+	Targets []string `json:"targets"`
+}
+
+/**
+This file opens the `config.json` file and reads the contents and returns the data.
+This is used by other functions to get the configurations.
+*/
+func getConfigurations() []Configuration {
+	configFile, error := os.Open("config.json")
+
+	// error while opening the file
+	if error != nil {
+		fmt.Println("Error while opening config file.", error)
+		os.Exit(3)
+	}
+
+	// read the file and get contents
+	defer configFile.Close()
+	byteValue, _ := ioutil.ReadAll(configFile)
+	var configurations []Configuration
+	json.Unmarshal(byteValue, &configurations)
+
+	return configurations
+}
 
 func main() {
 	// server instance
